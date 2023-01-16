3ab3c6961a772630e195ebb416d7aedf22546b2e
Ian Bailey <IanKulin@kulin.com.au>
2023-01-16 16:38:44 +0800

    ISS initial

---
+++ iss/script.js
@@ -0,0 +1,33 @@
+const lblLocation = document.querySelector("#lblLocation");
+
+function fetchISSLocation() {
+    try {
+        fetch("http://api.open-notify.org/iss-now.json")
+            .then(response => {
+                return response.json();
+            })
+            .then(location => {
+                updateLocation(location)
+            });
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+
+function updateLocation(location) {
+    const position = location.iss_position;
+    if (location.message === "success") {
+        lblLocation.innerHTML = `lat: ${position.latitude} long: ${position.longitude}`;
+    } else {
+        lblLocation.innerHTML = "not found";
+    };
+}
+
+
+function updateLoop() {
+    fetchISSLocation();
+    setTimeout(updateLoop, 5000);
+}
+
+updateLoop();
